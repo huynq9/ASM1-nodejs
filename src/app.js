b19da18ba57468f0df21dad6fb832facea9d2946
Scr18 <nguyenquanghuyx8@gmail.com>
2023-03-30 01:08:52 +0700

    update check quyen truy cap

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import productRouter from "./router/products";
+import cors from "cors";
+import authRouter from "./router/auth";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", productRouter);
+app.use("/api", authRouter);
 mongoose.connect("mongodb://127.0.0.1:27017/22032023");
 export const viteNodeApp = app;
--- src/router/products.js
+++ src/router/products.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { get, create, remove, update } from "../controllers/products";
+import { checkPermission } from "../middlewares/checkPermisson";
 const router = express.Router();
 
 router.get("/products/:id?", get);
-router.post("/products", create);
-router.delete("/products/:id", remove);
-router.patch("/products/:id", update);
+router.post("/products", checkPermission, create);
+router.delete("/products/:id", checkPermission, remove);
+router.patch("/products/:id", checkPermission, update);
 
 export default router;
